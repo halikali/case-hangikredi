0528264c02c4b8fdb0ae4fd82f5f39448eeccb4e
halikali <tunchalikali@gmail.com>
2022-07-14 01:50:08 +0300

    style the list widget component

---
+++ pages/list-widget.tsx
@@ -0,0 +1,21 @@
+import { NextPage } from "next";
+
+import ListWidget from "components/ListWidget";
+import style from "styles/ListWidget.page.module.css";
+
+const ListWidgetPage: NextPage = () => {
+  return (
+    <div className={style.container}>
+      <h1 className={style.title}>List widget</h1>
+      <p className={style.description}>
+        Aşağıda verilen input'a bir index değeri girerek widget'ın ilgili index
+        için gösterdiği aksiyonu görebilirsiniz. Listede 52 adet eleman
+        listelenmektedir. Eleman sayısından fazla bir index değeri girerseniz
+        liste en yukarı scroll olacaktır.
+      </p>
+      <ListWidget />
+    </div>
+  );
+};
+
+export default ListWidgetPage;
