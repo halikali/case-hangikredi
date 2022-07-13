876d7cbc55e7d8124ec2a5188e0939040f058ea3
halikali <tunchalikali@gmail.com>
2022-07-14 01:51:19 +0300

    provide by redux toolkit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,18 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
+import { Provider } from "react-redux";
+
+import Layout from "components/Layout";
+import store from "store";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Provider store={store}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </Provider>
+  );
 }
 
-export default MyApp
+export default MyApp;
